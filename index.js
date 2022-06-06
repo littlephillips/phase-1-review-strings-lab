944658c645c50eb0b8fb1e1fd8ef5beeea66e073
littlephillips <phillipkinuthia72@gmail.com>
2022-06-06 20:27:28 +0300

    edited inde.js file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+ let currentUser = 'Grace Hopper';
+
+ const welcomeMessage = `${currentUser}, Welcome to Flatbook, !`;
+ 
+ currentUser = `GRACE HOPPER`
+ const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+ currentUser = "Grace Hopper";
+
+ const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
